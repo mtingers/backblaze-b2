93dbffc462b813cae84153f1a9a1a4f3d1539968
HermogenesBatista <exemogenes@hotmail.com>
2017-07-06 15:00:15 -0300

    Fix typo with name of package and ignore all "egg-info"

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='blackblaze',
+    name='backblaze',
     version='0.1',
     packages=find_packages(),
     license=open("LICENSE").read(),
